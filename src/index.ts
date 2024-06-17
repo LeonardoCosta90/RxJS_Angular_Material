6a9ebef5a014ed3f3f46e1abc59b7a5a73998302
Leonardo <leonardo.everton@inatel.br>
2024-06-17 08:55:39 -0300

    feat: my firt instance of observable

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,30 @@
-import { from } from "rxjs";
+import { Observable, from } from "rxjs";
 
-let numbers = [1,5,10];
+let numbers = [1, 5, 10];
 let source = from(numbers);
+let sourceInstance = new Observable((Subscriber) => {
+  for (let n of numbers) {
+    if (n > 5) Subscriber.error("Aconteceu um erro esperado");
+    Subscriber.next(n);
+  }
+  Subscriber.complete();
+});
 
 class MyObserver {
   next(x: number) {
-    console.log(x)
+    console.log(x);
   }
   error(e: Error) {
-    console.log(e)
+    console.log(e);
   }
   complete() {
-    console.log('Complete')
+    console.log("Complete");
   }
 }
 
 function component() {
-  source.subscribe(new MyObserver)
+  source.subscribe(new MyObserver());
+  sourceInstance.subscribe(new MyObserver)
 }
 
-component()
+component();
